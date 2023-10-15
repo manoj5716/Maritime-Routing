39659d475a87b2e85bad90051b8c603a3fd4bd6b
ShaiBM123 <shaibenmichael@gmail.com>
2023-10-15 14:06:54 +0300

    sql query for testing - get optimal route and store it in table

--- MaritimeDataConversions/temp.sql
+++
@@ -1,2 +0,0 @@
-SELECT * INTO temp_route2 FROM get_optimal_one_to_one_route(-89.66227, 37.76118, -73.540627, 45.530268);
--- SELECT * FROM get_nearest_vertex_id(32.33, 34.11);
---
+++ MaritimeDataConversions/testing-get_optimal_route_and_store_it_in_tbl.sql
@@ -0,0 +1,2 @@
+SELECT * INTO temp_route3 FROM get_optimal_one_to_one_route(-64.2615, -43.2152, -239.541588, 35.768365);
+-- SELECT * FROM get_nearest_vertex_id(32.33, 34.11);
